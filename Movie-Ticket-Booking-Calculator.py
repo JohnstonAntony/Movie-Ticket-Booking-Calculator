25ece62e32a9959a9d75f3493ff7470bba45c365
Johnston Antony <Johnston4u19@gmail.com>
2026-02-02 20:39:08 +0000

    Added If function to validate if they fit the criteria to watch at different show times.

--- Movie-Ticket-Booking-Calculator.py
+++ Movie-Ticket-Booking-Calculator.py
@@ -6,13 +6,13 @@ show_time = "Evening"
 if age > 17: 
     print ("User is eligible to book a ticket")
 
-    if age >= 21: 
-        print ("User is eligible for Evening shows")
+if age >= 21: 
+    print ("User is eligible for Evening shows")
 
-    else: 
-        print ("User is not eligible for Evening shows")
+else: 
+    print ("User is not eligible for Evening shows")
 
-is_member = True 
+is_member = False 
 is_weekend = False
 
 discount = 0 
@@ -26,4 +26,20 @@ else:
 
 print ("Discount:", discount,)
 
- 
+extra_charges = 0 
+
+if is_weekend or show_time == "Evening":
+    extra_charges = 2
+    print ("Extra charges will be applied")
+
+else:
+    print ("No extra charges will be applied")
+
+print ("Extra charges:", extra_charges)
+
+
+if age >= 21 or age >= 18 and (show_time != "Evening" or is_member):
+    print ("Ticket booking condition satisfied")
+
+else:
+    print ("Ticket booking failed due to restrictions")
